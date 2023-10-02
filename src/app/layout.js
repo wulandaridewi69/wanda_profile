8d1a99312af986db64994268202ca64d6cbeeea5
w.dewi@global-coding.com <w.dewi@global-coding.com>
2023-10-02 10:59:10 +0700

    feature-footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Header/>
       <body className={inter.className}>{children}</body>
+      <Footer/>
     </html>
   )
 }
