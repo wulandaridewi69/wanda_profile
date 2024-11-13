ab6bf77e6d0d2397afb68f065206417713e72630
wulandaridewi69 <dewi.wulandari.ruslan@gmail.com>
2024-11-13 11:19:39 +0700

    update package dkk

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
  
 module.exports = {
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
